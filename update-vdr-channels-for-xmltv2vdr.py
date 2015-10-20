109b030eab2d53130e11742e9929986232b0f2bb
ibibah <ibibawal@gmail.com>
2015-10-20 15:56:04 +0200

    en cours de dev

--- update-vdr-channels-for-xmltv2vdr.py
+++ update-vdr-channels-for-xmltv2vdr.py
@@ -11,64 +11,79 @@ class ChannelHandler( xml.sax.ContentHandler ):
   def __init__(self):
     self.CurrentData = ""
     self.ChannelId = ""
-  
+
   # Call when an element starts
   def startElement(self, tag, attributes):
     self.CurrentData = tag
     if tag == "channel":
         self.ChannelId = attributes["id"]
-  
+
   # Call when an elements ends
   def endElement(self, tag):
     self.CurrentData = ""
-  
+
   # Call when a character is read
   def characters(self, content):
     if self.CurrentData == "display-name":
-      ChannelIds[content] = self.ChannelId 
+      chanIds = list()
+      if ChannelIds.has_key(self.ChannelId):
+        chanIds = ChannelIds[self.ChannelId]
+      chanIds.append(content.lower())
+      ChannelIds[self.ChannelId] = chanIds
 
 def process(src, dst):
 
   for ligne in src:
     donnees = ligne.rstrip('\n\r').split(":")
-    channelName = donnees[0].split(";")[0]
-    if ChannelIds.has_key(channelName):
-      chanId = ChannelIds[channelName]
+    channelName = (donnees[0].split(";")[0]).lower()
+    results = list()
+    for chan in ChannelIds:
+      ok = 0
+      for chanName in chan[1]:
+        if chanName == channelName:
+          ok = 1
+      if ok == 1:
+        results.append(chan)
+
+    if len(results) == 1 :
+      chanId = results[0][0];
       dst.write("%s:%s\n" %(ligne.rstrip('\n\r'), chanId ))
       print channelName + ' updated'
-    else:
+    elif len(results) > 1:
+      print channelName + ' plusieurs correspondance' + '->' + results.__str__()
+    elif len(results) == 0:
       print channelName + ' not found'
-        
+
 if ( __name__ == "__main__"):
-  parser = argparse.ArgumentParser(description='ajout des channelsId xmlttv dans channels.conf') 
+  parser = argparse.ArgumentParser(description='ajout des channelsId xmlttv dans channels.conf')
   parser.add_argument('-c','--channels', help='Input channels.conf',required=True)
   parser.add_argument('-x','--xmltv', help='Input channels.xml',required=True)
   parser.add_argument('-o','--output', help='Output channels.conf',required=True)
   args = parser.parse_args()
-  
+
   # create an XMLReader
   parser = xml.sax.make_parser()
   # turn off namespaces
   parser.setFeature(xml.sax.handler.feature_namespaces, 0)
-  
+
   # override the default ContextHandler
   Handler = ChannelHandler()
   parser.setContentHandler( Handler )
-  
+
   parser.parse(args.xmltv)
-  
+
   # Ouverture du fichier source
   source = open(args.channels, "r")
-    
+
   # Ouverture du fichier destination
   destination = open(args.output, "w")
-    
+
   try:
       # Appeler la fonction de traitement
       process(source, destination)
   finally:
       # Fermeture du fichier destination
       destination.close()
-    
+
       # Fermerture du fichier source
       source.close()
